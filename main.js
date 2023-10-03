e03254896118a10e835159e1929ad2db0612325e
MariaFelema <105368426+MariaFelema@users.noreply.github.com>
2023-10-03 11:22:28 -0300

    oiiii

--- main.js
+++ main.js
@@ -1,7 +1,103 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
     }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_pom');
 
-document.querySelector('#som_tecla_pom').play();
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+    }
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+    listaDeTeclas[0].onclick = tocaSomPom;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[1].onclick = tocaSomClap;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[2].onclick = tocaSomTim;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[3].onclick = tocaSomPuff;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[4].onclick = tocaSomSplash;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[5].onclick = tocaSomToim;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[6].onclick = tocaSomPsh;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[7].onclick = tocaSomTic;
+    while() {
+    }
+    let contador = 0;
+    listaDeTeclas[8].onclick = tocaSomTom;
+    while() {
+    }
+    let contador = 0;
+
+
+
+
