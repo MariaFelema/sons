dab0de0e53a3b911a8543bd48b21e72ed370cdfe
MariaFelema <105368426+MariaFelema@users.noreply.github.com>
2023-10-17 11:04:27 -0300

    comentarios

--- main.js
+++ main.js
@@ -1,103 +1,13 @@
+//Criando/declarando a função tocaSomPom.
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
     }
+
+//Chamando/invocando a função tocaSomPom pelo clique do botão pom.
+    document.querySelectorAll('.tecla').onclick = tocaSomPom
+
+//Criando referência constante listaDeTeclas buscando todos os itens pela classe .tecla.
     const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
-
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomTim(){
-    document.querySelector('#som_tecla_tim').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomPuff(){
-    document.querySelector('#som_tecla_puff').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomToim(){
-    document.querySelector('#som_tecla_toim').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomPsh(){
-    document.querySelector('#som_tecla_psh').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomTic(){
-    document.querySelector('#som_tecla_tic').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
-function tocaSomTom(){
-    document.querySelector('#som_tecla_tom').play();
-    }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
+//Invocando a função tocaSomPom a partir do item "0", 1º botão.
     listaDeTeclas[0].onclick = tocaSomPom;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[1].onclick = tocaSomClap;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[2].onclick = tocaSomTim;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[3].onclick = tocaSomPuff;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[4].onclick = tocaSomSplash;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[5].onclick = tocaSomToim;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[6].onclick = tocaSomPsh;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[7].onclick = tocaSomTic;
-    while() {
-    }
-    let contador = 0;
-    listaDeTeclas[8].onclick = tocaSomTom;
-    while() {
-    }
-    let contador = 0;
-
-
-
-
