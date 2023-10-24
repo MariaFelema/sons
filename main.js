2c10275e2835251875e629312d3a5edef98e3d10
MariaFelema <105368426+MariaFelema@users.noreply.github.com>
2023-10-24 11:25:07 -0300

    AI MEU DEDO

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-//Criando/declarando a função tocaSomPom.
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+//Criando/declarando a função tocaSom com parâmetro idElementoAudio
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
     }
 
 //Chamando/invocando a função tocaSomPom pelo clique do botão pom.
-    document.querySelectorAll('.tecla').onclick = tocaSomPom;
+    document.querySelectorAll('.tecla_pom').onclick = tocaSomPom;
 
 //Criando referência constante listaDeTeclas buscando todos os itens pela classe .tecla.
     const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,8 +16,12 @@ function tocaSomPom(){
     let contador = 0;
 
 //Estrutura de repetição "while" significa "enquanto algo acontecer"
-while(contador < 9){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+ efeito; 
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
     contador = contador + 1;
     console.log(contador);
 }
