d6f9c9d9e7c0c1040403b0e9bab6ff110aab0594
MariaFelema <105368426+MariaFelema@users.noreply.github.com>
2023-10-24 11:26:55 -0300

    iscreveia

--- main.js
+++ main.js
@@ -3,14 +3,8 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
     }
 
-//Chamando/invocando a função tocaSomPom pelo clique do botão pom.
-    document.querySelectorAll('.tecla_pom').onclick = tocaSomPom;
-
 //Criando referência constante listaDeTeclas buscando todos os itens pela classe .tecla.
     const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//Invocando a função tocaSomPom a partir do item "0", 1º botão.
-    listaDeTeclas[0].onclick = tocaSomPom;
 
 //Criando referência variável "contador" iniciando = 0
     let contador = 0;
